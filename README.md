98b242f7fbb01633c35e40654b0c36348e2dbf99
cmomodo <ceesay.ml@outlook.com>
2025-09-24 15:47:02 +0100

    This works for now but i have to fix the internal error more debugging to be done

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 we will explore what we have beeen learning about Docker.
 
 This will also contain all the projects we create in our journey.
+
+The first step was to learn about the basics.
+
+Next we will apply our knowledge in creating a basic flask web app.
+
+we will then containerise it and test it out.
+
+## BACKEND 
+we will then go into docker networking.
+
+This means we will be using multiple containers to communicate with one another.
--- hello_flask/app.py
+++ hello_flask/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import pymysql
 
 
 
@@ -6,7 +7,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, world!'
 
-if  __name__ == '__main__':
+    #connect to the sql database
+    db = pymysql.connect(
+        host="mydb",
+        user="root",
+        passwd="my-secret-pw",
+        db="mysql"
+    )
+
+    cur = db.cursor()
+    cur.execute("SELECT VERSION()")
+    version = cur.fetchone()
+    return f'Hello, world mysql version : {version[0]}'
+    
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
