2e9a2b11a14cd3b2cc8b4196cc06cb05dcf55537
cmomodo <ceesay.ml@outlook.com>
2025-09-25 11:13:43 +0100

    feat: Add Docker support for Flask application

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -1,26 +1,23 @@
+# app.py
+
 from flask import Flask
-import pymysql
-
-
+import MySQLdb
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-
-    #connect to the sql database
-    db = pymysql.connect(
-        host="mydb",
-        user="root",
-        passwd="my-secret-pw",
-        db="mysql"
+    # Connect to the MySQL database
+    db = MySQLdb.connect(
+        host="mydb",    # Hostname of the MySQL container
+        user="root",    # Username to connect to MySQL
+        passwd="my-secret-pw",  # Password for the MySQL user
+        db="mysql"      # Name of the database to connect to
     )
-
     cur = db.cursor()
     cur.execute("SELECT VERSION()")
     version = cur.fetchone()
-    return f'Hello, world mysql version : {version[0]}'
-    
+    return f'Hello, World! MySQL version: {version[0]}'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
